a8c76b119b1e3e3753d196a3911604035ec480a3
mitch1259 <mmartinez8484@gmail.com>
2023-02-22 18:15:51 -0500

    Nav bar and footer, they need links added to their hrefs though

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import Home from "./pages/Home.js";
+import Footer from "./pages/Footer.js";
+import Header from "./pages/Header.js";
 
 function App() {
   return (
     <div className="App">
+        <Header />
         <Home />
+        <Footer />
     </div>
   );
 }
