ff93b22e2869b00f23a4e3ce18241619d54cb82b
Erik Lewis <llewis65@uncc.edu>
2023-02-23 20:29:10 -0500

    fixed merge conflict with app.js and package-lock.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import Allchart from "./pages/Allchart.js";
-import Home from "./pages/Home.js";
+import Header from '../src/pages/Header.js';
+import Footer from '../src/pages/Footer.js';
+import Login from '../src/pages/Login.js';
+import Registration from '../src/pages/Registration.js';
+import Profile from '../src/pages/Profile.js';
+import AccountDashboard from './pages/AccountDashboard.js';
+import AppRoutes from './router/AppRoutes.js';
 
 function App() {
   return (
     <div className="App">
-        <Allchart/>
+        <Header />
+        {/* <AppRoutes /> */}
+        <Login />
+        <Footer />
     </div>
   );
 }
