cbbe79193ea554e889b5eefa722cf583188ade30
erikiscoding <erikwfu14@gmail.com>
2023-04-02 16:55:40 -0400

    minor unit testing tweaks

--- server/__test__/userAPI.test.js
+++ server/__test__/userAPI.test.js
@@ -15,24 +15,3 @@ describe('USER LOGIN / REGISTRATION TESTS', () => {
     expect(response.data.length).toBeGreaterThan(0);
   });
 });
-
-
-
-// describe('USER LOGIN / REGISTRATION TESTS', () => {
-//     test('2. should register user for new account properly', async () => {
-//         const firstName = makeid(3);
-//         const lastName = makeid(3);
-//         const email = makeid(3);
-//         const password = makeid(3);
-//         const url = 'http://localhost:3002/api/registerUser';
-    
-//         const response = await axios.post(url, {
-//             firstName, lastName, email, password
-//         });
-    
-//         expect(response.status).toBe(200);
-//       })
-//   });
-
-
-
