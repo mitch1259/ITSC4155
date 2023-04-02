838620f6f244c0680ece5fc7715280858f2c1dc8
erikiscoding <erikwfu14@gmail.com>
2023-04-02 18:37:45 -0400

    added create savings goal to approutes, added some npm packages

--- client/src/router/AppRoutes.js
+++ client/src/router/AppRoutes.js
@@ -8,6 +8,7 @@ import Allchart from '../pages/Allchart.js';
 import EditProfile from '../pages/EditProfile.js';
 import SavingsBoard from '../pages/SavingsBoard.js';
 import PrivateRoutes from './PrivateRoutes.js';
+import CreateGoal from '../pages/CreateSavingGoal.js';
 // import AuthAPI from '../server/authentication/AuthAPI.js';
 
 function AppRoutes() {
@@ -22,6 +23,7 @@ function AppRoutes() {
           <Route path="/savings-boards" element={<Allchart />} />
           <Route path="/profile/editprofile" element={<EditProfile />} />
           <Route path="/savings-board/id" element={<SavingsBoard />} />
+          <Route path='/create-saving-goal' element={<CreateGoal/>} />
         </Route>
 
         {/* OPEN ROUTES -- anything listed in the below <Route> block is available to unauthenticated users */}
