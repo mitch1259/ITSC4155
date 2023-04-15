590553ec6e8f872f43ac47cb70b19cac9ebdf20e
mitch1259 <mmartinez8484@gmail.com>
2023-04-15 17:08:55 -0400

    board creation and deletion beginnings

---
+++ client/src/components/dashboard/CreateBoard.js
@@ -0,0 +1,109 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextField from '@mui/material/TextField';
+
+function CreateBoard(props) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  }
+
+  const handleClose = () => {
+    setName("");
+    setDesc("");
+    setBudget("");
+
+    setOpen(false);
+  }
+
+  const createBoard = () => {
+    handleClose();
+
+    //NEEDS LOGIC/AXIOS CALL TO DELETE BOARD
+  }
+
+  const sxFont = {
+    fontFamily: 'Barlow Condensed',
+    fontSize: '20px',
+    fontWeight: '500'
+  };
+
+  const sxButton = {
+    color: "red", fontFamily: "Barlow Condensed", backgroundColor: "antiquewhite", fontSize: "18px", textTransform: "none"
+  };
+
+  const sxButton2 = {
+    fontFamily: "Barlow Condensed", textTransform: "none", fontSize: "18px", backgroundColor: "lightgreen"
+  };
+
+  const [name, setName] = React.useState("");
+  const handleName = (event) => {
+    setName(event.target.value);
+  }
+
+  const [desc, setDesc] = React.useState("");
+  const handleDesc = (event) => {
+    setDesc(event.target.value);
+  }
+
+  const [budget, setBudget] = React.useState(0);
+  const handleBudget = (event) => {
+    setBudget(event.target.value);
+  }
+
+  return (
+    <div>
+      <button onClick={handleOpen} className='board-link-button'>Create Board</button>
+      <Dialog open={open} onClose={handleClose}>
+      <DialogTitle sx={sxFont}>Create Board</DialogTitle>
+        <DialogContent id='add-transaction-dialog-box'>
+          <DialogContentText className='add-transaction-form' sx={sxFont}>
+            <TextField
+              className='add-transaction-form'
+              autoFocus
+              margin="dense"
+              label="Board Name"
+              type="text"
+              fullWidth
+              variant="filled"
+              onChange={handleName}
+              required
+            />
+            <TextField
+              className='add-transaction-form'
+              margin="dense"
+              label="Board Description"
+              type="text"
+              fullWidth
+              variant="filled"
+              onChange={handleDesc}
+              required
+            />
+            <TextField
+              inputProps={{ type: 'number'}}
+              className='add-transaction-form'
+              margin="dense"
+              label="Weekly Budget"
+              type="text"
+              fullWidth
+              variant="filled"
+              onChange={handleBudget}
+              required
+            />
+          </DialogContentText>
+          <DialogActions>
+            <Button onClick={createBoard} sx={sxButton2}>Create Board</Button>
+            <Button onClick={handleClose} sx={sxButton}>Cancel</Button>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
+    </div>
+  )
+}
+
+export default CreateBoard
---
+++ client/src/components/dashboard/DeleteBoard.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+function DeleteBoard(props) {
+  const [ready, setReady] = React.useState(false);
+  const handleOpen = () => {
+    setReady(true);
+  }
+
+  const handleClose = () => {
+    setReady(false);
+  }
+
+  const deleteBoard = () => {
+    handleClose();
+
+    //NEEDS LOGIC/AXIOS CALL TO DELETE BOARD
+  }
+
+  const sxFont = {
+    fontFamily: 'Barlow Condensed',
+    fontSize: '20px',
+    fontWeight: '500'
+  };
+
+  const sxButton = {
+    color: "red", fontFamily: "Barlow Condensed", backgroundColor: "antiquewhite", fontSize: "18px", textTransform: "none"
+  };
+
+  const sxButton2 = {
+    fontFamily: "Barlow Condensed", textTransform: "none", fontSize: "18px", backgroundColor: "lightgreen"
+  };
+
+  return (
+    <div>
+      <Button onClick={handleOpen} sx={sxButton}>Delete Board</Button>
+      <Dialog open={ready} onClose={handleClose}>
+      <DialogTitle sx={sxFont}>Confirmation</DialogTitle>
+        <DialogContent id='add-transaction-dialog-box'>
+          <DialogContentText className='add-transaction-form' sx={sxFont}>
+            Are you sure you want to delete this board?
+
+            Note: This action cannot be undone
+          </DialogContentText>
+          <DialogActions>
+            <Button onClick={handleClose} sx={sxButton2}>Cancel</Button>
+            <Button onClick={deleteBoard} sx={sxButton}>Delete Board</Button>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
+    </div>
+  )
+}
+
+export default DeleteBoard
