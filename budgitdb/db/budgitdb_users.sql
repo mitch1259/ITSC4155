7a86568b91cb8ca2b9807258d6c9deacf88c0a2c
Matthew Short <107050961+mgshort0@users.noreply.github.com>
2023-05-01 22:45:18 -0400

    Update budgitdb_users.sql
    changed password requirements

--- budgitdb/db/budgitdb_users.sql
+++ budgitdb/db/budgitdb_users.sql
@@ -27,7 +27,7 @@ CREATE TABLE `users` (
   `firstName` varchar(45) NOT NULL,
   `lastName` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
+  `password` varchar(100) NOT NULL,
   PRIMARY KEY (`userID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
